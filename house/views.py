c718bd12a6619e51563946a1078393f6b44c8d2c
Xuechaohui <1126686819@qq.com>
2018-08-14 16:42:13 +0800

    第三次提交

--- house/views.py
+++ house/views.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def index_view(request):
+    return HttpResponse('成功')
